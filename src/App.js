e02ef8a3723d1cd31c169eb9c52a485ecf1f3d14
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-03-25 07:28:25 -0700

    last commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,66 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  return (
+    <div className="App">
+      <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+
+  // const incressCount = () => {
+  //   const newCount = count + 1;
+  //   setCount(newCount);
+  // };
+
+  return (
+    <div>
+      <h1>Count: {count} </h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  );
+}
+
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  // useEffect(()=> {},[])
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>External Users</h1>
+      <h3>{users.length}</h3>
+      {users.map((user) => (
+        <User name={user.name} email={user.email}></User>
+      ))}
+    </div>
+  );
+}
+
+function User(props) {
+  return (
+    <div>
+      <h1>Name: {props.name}</h1>
+      <p>Email: {props.email}</p>
+    </div>
+  );
+}
+
+/* 
   const products = [
     {
       name: "Laptop",
@@ -24,14 +83,12 @@ function App() {
       id: "AB4",
     },
   ];
-  return (
-    <div className="App">
+*/
+/*
       {products.map((product) => (
         <Product name={product.name} price={product.price} id={product.id}></Product>
       ))}
-    </div>
-  );
-}
+*/
 
 function Product(props) {
   return (
